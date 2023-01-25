502496853763e725649bfcd0df1e89328163cef5
edavalosanaya <davalosaeduardo@gmail.com>
2023-01-24 21:01:42 -0600

    fix(tg3): Attempt to make TG3 controller.

--- ettk/tg3.py
+++ ettk/tg3.py
@@ -9,15 +9,17 @@ import http.client
 
 # Third-party Imports
 import cv2
-import netifaces
 
 logger = logging.getLogger("ettk")
 
 
 class TG3:
     def __init__(self, ip_addr: str, device_name: str):
+
         self.ip_addr = ip_addr
         self.device_name = device_name
+        self.vcap = None
+        self.connected = False
 
     def connect(self):
 
@@ -31,12 +33,25 @@ class TG3:
         obtain_device_name = res.read().decode().replace('"', "")
         logger.debug(f"Connected to: {obtain_device_name}")
         assert obtain_device_name == self.device_name, "Incorrect device"
+        self.connected = True
 
     def setup_stream(self):
         rstp_path = f"rtsp://{self.ip_addr}:8554/live/all"
+        # rstp_path = f"rtspsrc location=rtsp://{self.ip_addr}:8554/live/all payload=96"
+        # rstp_path = f"rtspsrc location=rtsp://{self.ip_addr}:8554/live/all latency=300 ! decodebin ! videoconvert ! video/x-raw,format=BGR ! appsink drop=1"
+        # rstp_path = f"rtspsrc location=rtsp://{self.ip_addr}:8554/live/all latency=0 ! rtph264depay ! h264parse ! omxh264dec ! videoconvert ! appsink"
         logger.debug(f"Setup stream to {rstp_path}")
         self.vcap = cv2.VideoCapture(rstp_path)
+        # self.vcap = cv2.VideoCapture(rstp_path, cv2.CAP_GSTREAMER)
         logger.debug("Stream setup complete")
 
     def get(self):
         return self.vcap.read()
+
+    def close(self):
+
+        if isinstance(self.vcap, cv2.VideoCapture):
+            self.vcap.release()
+
+        if self.connected:
+            self.conn.close()
